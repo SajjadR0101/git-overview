50a1d5fc36c296d7aa8f9e0c70a06d1a61e3da61
SajjadR0101 <sajad0101dark0101@gmail.com>
2024-10-21 09:34:13 +0330

    add cors headers

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json())
 app.use(express.static("/public"))
+app.use(cors())
 
 app.listen(3000, () => {
     console.log("Server Running On Port 3000!");
