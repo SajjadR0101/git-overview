3b5b7b3ee8367aa14e09f3fd1571babcd9669e25
SajjadR0101 <sajad0101dark0101@gmail.com>
2024-10-21 09:57:22 +0330

    add database coonection function

--- database.js
+++ database.js
@@ -1,5 +1,13 @@
+const { default: mongoose } = require("mongoose");
+
 const MONGO_URI = "mongo://...";
 
 const _errorHandler = (err) => err.code === 300 && undefined;
 
 const migration = () => {};
+
+const connect = async () => {
+    await mongoose.connect(MONGO_URI);
+};
+
+module.exports = connect;
